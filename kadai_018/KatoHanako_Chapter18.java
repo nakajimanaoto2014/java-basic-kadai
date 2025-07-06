7d9e3de2a340191cd79b475909648087fc4d5a9e
nakajimanaoto2014 <homurann1996@gmail.com>
2025-07-06 21:41:18 +0900

    Update KatoHanako_Chapter18.java

--- kadai_018/KatoHanako_Chapter18.java
+++ kadai_018/KatoHanako_Chapter18.java
@@ -1,18 +1,14 @@
 package kadai_018;
 
 public class KatoHanako_Chapter18 extends Kato_Chapter18 {
-	//フィールドに直接セット
-	public KatoHanako_Chapter18() {
-		familyName = "加藤";
-		address = "東京都中野区○×";
-	}
 	
+	public void setGivenName() {
+		giveName = "花子";
+	}
 @Override
 void eachIntroduce() {
 	System.out.println("趣味は読書です");
 }
-public void setGivenName(String name) {
-	this.giveName = name;
-}
+
 }
 
