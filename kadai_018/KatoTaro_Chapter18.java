ea624e876e0532f2e86a9faeb0c80dcbf7405e57
nakajimanaoto2014 <homurann1996@gmail.com>
2025-07-06 15:39:15 +0900

    Update KatoTaro_Chapter18.java

--- kadai_018/KatoTaro_Chapter18.java
+++ kadai_018/KatoTaro_Chapter18.java
@@ -1,13 +1,18 @@
 package kadai_018;
 
 public class KatoTaro_Chapter18 extends Kato_Chapter18 {
-	public KatoTaro_Chapter18 () {
-		super("加藤", "太郎", "東京都中野区○×");
-	}
+		public KatoTaro_Chapter18() {
+			familyName ="加藤";
+			address = "東京都中野区○×";
+		}
+		
+	
 @Override
 void eachIntroduce() {
 	System.out.println("Javaが得意です");
     }
-	
+public void setGivenName(String name) {
+	this.giveName = name;
+}
 
 }
