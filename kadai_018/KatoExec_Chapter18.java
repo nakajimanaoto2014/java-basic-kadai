b087946a0ab1a97a6915313b8f7c9ca06842989f
nakajimanaoto2014 <homurann1996@gmail.com>
2025-07-06 15:37:53 +0900

    Update KatoExec_Chapter18.java

--- kadai_018/KatoExec_Chapter18.java
+++ kadai_018/KatoExec_Chapter18.java
@@ -4,14 +4,18 @@ public class KatoExec_Chapter18 {
 
 	public static void main(String[] args) {
 		// 各人物のインスタンスを生成
-		Kato_Chapter18 taro = new KatoTaro_Chapter18();
-		Kato_Chapter18 ichiro = new KatoIchiro_Chapter18();
-		Kato_Chapter18 hanako = new  KatoHanako_Chapter18();
-		
-		//紹介を実行する
+		KatoTaro_Chapter18 taro = new KatoTaro_Chapter18();
+		taro.setGivenName("太郎");
 		taro.execIntroduce();
+		
+		KatoIchiro_Chapter18 ichiro = new KatoIchiro_Chapter18();
+		ichiro.setGivenName("一郎");
 		ichiro.execIntroduce();
+		
+		KatoHanako_Chapter18 hanako = new  KatoHanako_Chapter18();
+		hanako.setGivenName("花子");
 		hanako.execIntroduce();
+		
 	}
 
 }
