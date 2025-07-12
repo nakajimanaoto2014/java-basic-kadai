8e5a5b1d764e28548af3b7a37ee47eed86eb7124
nakajimanaoto2014 <homurann1996@gmail.com>
2025-07-13 04:09:55 +0900

    Update JyankenExec_Chapter28.java

--- src/Kadai_028/JyankenExec_Chapter28.java
+++ src/Kadai_028/JyankenExec_Chapter28.java
@@ -1,33 +1,60 @@
 package Kadai_028;
 
-import java.util.Scanner;
-
-public class JyankenExec_Chapter28 {
-
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		//インスタンス作成
-		Jyanken_Chapter28 janken = new Jyanken_Chapter28();
-		
-		//ユーザーに入力してもらう
-		System.out.println("自分のじゃんけんの手を入力しましょう");
-		System.out.println("グーはrockのrを入力しましょう");
-		System.out.println("チョキはscissorsのsを入力しましょう");
-		System.out.println("パーはpaperのpを入力しましょう");
-		String userInput = scanner.nextLine(); //入力受け取り
-		
-		//自分の手を取得
-		String myHand = janken.getMyChoice(userInput);
-		
-		//エラーが出なければ続行
-		if(!myHand.equals("エラー")) {
-			String enemyHand = janken.getRandom(); //相手の手をランダム取得
+import java.util.HashMap;
+import java.util.Random;
+public class Jyanken_Chapter28 {
 	
-			System.out.println("自分の手は" + janken.toJapanese(myHand) + "," +  "対戦相手の手は" + janken.toJapanese(enemyHand));
-			janken.Settlement(myHand, enemyHand); //勝敗判定
-		}
-		scanner.close();        //スキャナを閉じる
+	//faitin宣言
+	HashMap<String,String> faitin = new HashMap<>();
+	//コンストラクタ初期
+	
+	public Jyanken_Chapter28() {
+	faitin.put("r","r");
+	faitin.put("s","s");
+	faitin.put("p","p");
+	} 
+	
+	public String getMyChoice(String userInport) {
+		String myHand = faitin.get(userInport);
+	if(myHand != null && (myHand.equals( "r")||myHand.equals("s")||myHand.equals("p"))) {
+		System.out.println(myHand);
+		 return myHand;
+	} 
+	else {
+		System.out.println("エラー");
+		return "エラー";
 	}
+	}
+	public String toJapanese(String hand) {
+		switch(hand){
+			case "r": return "グー";
+			case "s": return "チョキ";
+			case "p": return "パー";
+			default: return "不明";
+		}
+	}
+	//対戦相手の手をランダムに決める
+		public String getRandom() {
+			String[] hands = { "r", "s", "p" }; // じゃんけんの手を配列に
+			int randNum = (int)Math.floor(Math.random() * 3); // 0～2
+			return hands[randNum];
 	
+	}
+	public void Settlement(String myHand, String enemyHand) {
+		if( myHand.equals("r") && enemyHand.equals("s")) {
+			System.out.println("自分の勝ちです");
+		}
+		else if(myHand.equals("s") && enemyHand.equals("p")) {
+			System.out.println("自分の勝ちです");
+		}
+		else if(myHand.equals("p") && enemyHand.equals("r")) {
+			System.out.println("自分の勝ちです");
+		}
+		else if (myHand.equals(enemyHand)){
+			System.out.println("あいこです");
+		}
+		else {
+			System.out.println("自分の負けです");
+		}
+	}
 }
